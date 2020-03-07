d1c756fddd2b8f731e196975ce739457d6fdd477
rreyes <raulreyes23ss@gmail.com>
2020-03-06 22:33:27 -0400

    Unit test

--- src/main/java/com/raulreyes/restApi/controller/UserRestController.java
+++ src/main/java/com/raulreyes/restApi/controller/UserRestController.java
@@ -17,6 +17,7 @@ public class UserRestController {
 
     @GetMapping("/users")
     public List<User> findAll(){
+
         return userService.findAll();
     }
 
--- src/test/java/com/raulreyes/restApi/user/userControllerTest.java
+++ src/test/java/com/raulreyes/restApi/user/userControllerTest.java
@@ -1,27 +1,54 @@
 package com.raulreyes.restApi.user;
-
-import com.raulreyes.restApi.controller.UserRestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.raulreyes.restApi.entity.User;
 import com.raulreyes.restApi.service.UserService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(UserRestController.class)
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class userControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-
     @MockBean
-    private UserService userService;
+    UserService userService;
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("Test Spring @Autowired Integration")
     @Test
-    public  void testExamples() throws Exception{
+    void testGetListUser() throws Exception {
+        User user = new User();
+        user.setId(2);
+        user.setEmail("dfd");
+        user.setPassword("aaaaa");
+        user.setCreateAt(new Date());
+        user.setUpdatedAt(new Date());
 
+        List<User> users = Arrays.asList(user);
+
+        given(userService.findAll()).willReturn(users);
+
+        mockMvc.perform(get("/api/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(2)));
     }
 }
